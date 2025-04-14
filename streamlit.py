825431380b41927729bdec69ee2dd853fcfe08c1
Harsha MJ <mall0247@algonquinlive.com>
2025-04-13 20:10:47 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -11,15 +11,19 @@ This app predicts whether a loan applicant is eligible for a loan
 based on various personal and financial characteristics.
 """)
 
-
+# # Optional password protection (remove if not needed)
+# password_guess = st.text_input("Please enter your password?")
+# # this password is stores in streamlit secrets
+# if password_guess != st.secrets["password"]:
+#     st.stop()
 
 # Load the pre-trained model
-rf_pickle = open("random_forest_credit.pickle", "rb")
+rf_pickle = open("models/RFmodel.pkl", "rb")
 rf_model = pickle.load(rf_pickle)
 rf_pickle.close()
 
 
-# Prepare the form for individual predictions
+# Prepare the form to collect user inputs
 with st.form("user_inputs"):
     st.subheader("Loan Applicant Details")
     
@@ -71,64 +75,52 @@ with st.form("user_inputs"):
     submitted = st.form_submit_button("Predict Loan Eligibility")
 
 
+# Handle the dummy variables to pass to the model
+if submitted:
+    Gender_Male = 0 if Gender == "Female" else 1
+    Gender_Female = 1 if Gender == "Female" else 0
 
+    Married_Yes = 1 if Married == "Yes" else 0
+    Married_No = 1 if Married == "No" else 0
 
-Gender_Male = 0
-if Gender == "Male":
-    Gender_Male = 1
-elif Gender == "Female":
-    Gender_Male = 0
+    # Handle dependents
+    Dependents_0 = 1 if Dependents == "0" else 0
+    Dependents_1 = 1 if Dependents == "1" else 0
+    Dependents_2 = 1 if Dependents == "2" else 0
+    Dependents_3 = 1 if Dependents == "3+" else 0
 
-Married_Yes = 0
-if Married == "Yes":
-    Married_Yes = 1
-elif Married == "No":
-    Married_Yes = 0
+    Education_Graduate = 1 if Education == "Graduate" else 0
+    Education_Not_Graduate = 1 if Education == "Not Graduate" else 0
 
-Dependents_1,Dependents_2,Dependents_3 = 0,0,0
-if Dependents == 1:
-    Dependents_1 = 1
-elif Dependents == 2:
-    Dependents_2 = 1
-elif Dependents == 3:
-    Dependents_3 = 1
+    Self_Employed_Yes = 1 if Self_Employed == "Yes" else 0
+    Self_Employed_No = 1 if Self_Employed == "No" else 0
 
-Education_Not_Graduate = 0
-if Education == "Graduate":
-    Education_Not_Graduate = 0
-elif Education == "Not_Graduate":
-    Education_Not_Graduate = 1
+    Property_Area_Rural = 1 if Property_Area == "Rural" else 0
+    Property_Area_Semiurban = 1 if Property_Area == "Semiurban" else 0
+    Property_Area_Urban = 1 if Property_Area == "Urban" else 0
 
-Self_Employed_Yes = 0
-if Self_Employed == "Yes":
-    Self_Employed_Yes = 1
-elif Self_Employed == "No":
-    Self_Employed_Yes = 0
+    # Convert Loan Amount Term and Credit History to integers
+    Loan_Amount_Term = int(Loan_Amount_Term)
+    Credit_History = int(Credit_History)
 
+    # Prepare the input for prediction. This has to go in the same order as it was trained
+    prediction_input = [[ApplicantIncome, CoapplicantIncome, LoanAmount,
+        Loan_Amount_Term, Credit_History, Gender_Female, Gender_Male,
+        Married_No, Married_Yes, Dependents_0, Dependents_1,
+        Dependents_2, Dependents_3, Education_Graduate,
+        Education_Not_Graduate, Self_Employed_No, Self_Employed_Yes,
+        Property_Area_Rural, Property_Area_Semiurban, Property_Area_Urban
+    ]]
 
-Property_Area_Semiurban, Property_Area_Urban = 0,0
-if Property_Area == "Semiurban":
-    Property_Area_Semiurban = 1
-elif Property_Area == "Urban":
-    Property_Area_Urban == 1
-    
+    # Make prediction
+    new_prediction = rf_model.predict(prediction_input)
 
-new_prediction = rf_model.predict(
-    [[ Dependents, ApplicantIncome, CoapplicantIncome, LoanAmount,
-       Loan_Amount_Term, Credit_History, Gender_Male, Married_Yes,
-       Education_Not_Graduate, Self_Employed_Yes,
-       Property_Area_Semiurban, Property_Area_Urban
-     ]]
-)
-
-st.subheader("Predicting the outcome:")
-# predicted_outcome = new_prediction[0]
-if new_prediction[0] == 'Y':
-    st.write("You are eligible for the loan!")
-else:
-    st.write("Sorry, you are not eligible for the loan.")
-
-
+    # Display result
+    st.subheader("Prediction Result:")
+    if new_prediction[0] == 'Y':
+        st.write("You are eligible for the loan!")
+    else:
+        st.write("Sorry, you are not eligible for the loan.")
 
 st.write(
     """We used a machine learning (Random Forest) model to predict your eligibility, the features used in this prediction are ranked by relative
